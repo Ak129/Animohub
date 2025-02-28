d836618aafd87eab2a104a59a93cc5351b212b8d
Ak129 <64674981+Ak129@users.noreply.github.com>
2025-02-28 23:40:48 +0530

    Update AnimohubU.js

--- AnimohubU.js
+++ AnimohubU.js
@@ -1,6 +1,6 @@
 {
-  "title": "ggg",
-  "dis": "uejdjdj",
-  "link": "wiieie",
+  "title": "❗NOTICE❗",
+  "dis": "Must Join Our Telegram Channel To Get a Latest Updates",
+  "link": "https://telegram.dog/animohub",
   "show": true
 }
