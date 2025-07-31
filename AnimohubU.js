f002204090602161bd459458f7a59f23dda143da
Ak129 <64674981+Ak129@users.noreply.github.com>
2025-07-31 16:09:45 +0530

    Update AnimohubU.js

--- AnimohubU.js
+++ AnimohubU.js
@@ -3,6 +3,6 @@
   "version_name": "2.9",
   "apk_url": "https://animohubapk.com/",
   "update_message": "• Fix Some Bugs",
-  "is_mandatory": true,
-  "show_later": false
+  "is_mandatory": false,
+  "show_later": true
 }
